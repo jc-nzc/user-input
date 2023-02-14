4f2df9552538c1d4cd9f9e0a4b91c86ea49b6af9
Zero <node.zerocool@gmail.com>
2023-02-13 23:59:52 -0600

    updated template

--- script.js
+++ script.js
@@ -1,49 +1,31 @@
-// Get the form element
-var form = document.getElementById("myForm");
-
-// Listen for the form submission
-form.addEventListener("submit", function (e) {
-  // Prevent the form from being submitted
-  e.preventDefault();
+function formData(){
   console.log("Form Submitted");
 
-  // Example form post, not fully built, just testing in the console
   fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     body: JSON.stringify({
-      name: "Logan Garcia",
-      email: "user@domain.com",
-      hand_preference: "Ambidexstrous",
-      file: "image.png",
-      userId: 3
+      name: document.getElementById("fName").value,
+      email: document.getElementById("lName").value,
+      phone: document.getElementById("phone").value,
+      email: document.getElementById("email").value,
     }),
     headers: {
       "Content-type": "application/json; charset=UTF-8"
     }
   })
-    .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      }
-      return Promise.reject(response);
-    })
-    .then(function (data) {
-      console.log(data);
-    })
-    .catch(function (error) {
-      console.warn("Something went wrong.", error);
-    });
-});
-
-
-
-
-// function formdata()
-// {
-// 	var firstname= document.getElementById("firstname1").value;
-// 	var lastname= document.getElementById("lastname1").value;
-// 	document.writeln("<h1>Confirmation Page</h1><br>");
-// 	document.writeln("Thank you for completing this form.<br><br>");
-// 	document.writeln("The first name you entered is " + firstname);
-// 	document.writeln("The last name you entered is " + lastname);
-// }
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(response);
+  })
+  .then(function (data) {
+    console.log(data);
+    document.getElementById("response").innerHTML = "Success!";
+  })
+  .catch(function (error) {
+    console.warn("Something went wrong.", error);
+    document.getElementById("response").innerHTML = "Error!";
+  });
+};
+console.log("sanity check");
